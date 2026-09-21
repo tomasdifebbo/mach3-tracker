14513f73f70dbe2a38a39bee79a88793a79d667c
Tomas Di Febbo <tomas@example.com>
2026-09-21 20:07:45 -0300

    fix svg download

--- dashboard-v2/src/components/GeradorLetraCaixa3D.jsx
+++ dashboard-v2/src/components/GeradorLetraCaixa3D.jsx
@@ -368,6 +368,7 @@ export function GeradorLetraCaixa3D() {
     const a = document.createElement('a');
     a.href = url;
     a.download = filename;
+    a.target = '_blank';
     document.body.appendChild(a);
     a.click();
     document.body.removeChild(a);
